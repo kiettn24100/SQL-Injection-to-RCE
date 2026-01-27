dcc335e96d40e8f9392225eeab754ed88058b839
kiettn24100 <kietnguyen24100@gmail.com>
2026-01-27 17:07:48 +0700

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS users (
 
 
 INSERT IGNORE INTO users (username, password) VALUES ('admin', 'flag{sqli_injection}');
+
+
+
+-- tạo ra user01 , password 12345678
+CREATE USER IF NOT EXISTS 'user01'@'%' IDENTIFIED BY '12345678';
+GRANT ALL PRIVILEGES ON sqllab.* TO 'user01'@'%';
+-- cấp quyền FILE , các bạn cứ hiểu như user thường kết nối với db chỉ được dùng các câu lệnh như union , select , update ,...
+-- nếu dùng INTO OUTFILE là nó lỗi ngay
+GRANT FILE ON *.* TO 'user01'@'%'; 
+FLUSH PRIVILEGES;
